49ed9298c981c57b966d4e93323f4517e5340a4e
Siddhant Kasat <siddhantkasat@gmail.com>
2017-07-14 10:31:24 -0400

    With Total asset

--- routes/chain.js
+++ routes/chain.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 const chain = require('chain-sdk')
 const client = new chain.Client()
+var googleStocks = require('google-stocks');
+var fetchQuotes = require('yahoo-finance-quotes');
+var express = require('express');
+var router = express.Router();
 
 let _signer
 
@@ -20,18 +24,24 @@ router.post('/addAccount', function(req, res) {
     _signer = signer
     return key
     }).then(key => {
-  
+
     // snippet create-asset: STOCK 1
     const goldPromise = client.assets.create({
-      alias: req.body.Stock1,
+      alias: (req.body.Stock1).replace(/\s+/g, ''),
       rootXpubs: [key.xpub],
       quorum: 1,
     })
     // endsnippet
-    
+
     // snippet create-asset: STOCK 2
     const silverPromise = client.assets.create({
-      alias: req.body.Stock2,
+      alias: (req.body.Stock2).replace(/\s+/g, ''),
+      rootXpubs: [key.xpub],
+      quorum: 1,
+    })
+    //snippet create-asset: NameTotal (To store total collateral)
+    const totalPromise = client.assets.create({
+      alias: req.body.firstName + req.body.lastName+"Total",
       rootXpubs: [key.xpub],
       quorum: 1,
     })
@@ -43,48 +53,76 @@ router.post('/addAccount', function(req, res) {
       quorum: 1
     })
     // endsnippet
-    
-    return Promise.all([goldPromise, silverPromise, alicePromise]);
+
+    return Promise.all([goldPromise, silverPromise, alicePromise,totalPromise]);
     res.send({msg:''});
-    }).then( t => { 
-    
+    }).then( t => {
+
     //TRANSACTIONS:
     //Putting the first stock on the ledger
     client.transactions.build(builder => {
         builder.issue({
-            assetAlias: req.body.Stock1,
+            assetAlias: (req.body.Stock1).replace(/\s+/g, ''),
             amount: parseInt(req.body.quantity1)
         })
         builder.controlWithAccount({
             accountAlias: req.body.firstName + " " + req.body.lastName,
-            assetAlias: req.body.Stock1,
+            assetAlias: (req.body.Stock1).replace(/\s+/g, ''),
             amount: parseInt(req.body.quantity1)
         })
     }).then(issuance => {
         return _signer.sign(issuance)
     }).then(signed => {
         return client.transactions.submit(signed)
-    })  
-  }).then( v => { 
+    })
+  }).then( v => {
 
-    //Putting the second stock on the ledger 
+    //Putting the second stock on the ledger
     client.transactions.build(builder => {
         builder.issue({
-            assetAlias: req.body.Stock2,
+            assetAlias: (req.body.Stock2).replace(/\s+/g, ''),
             amount: parseInt(req.body.quantity2)
         })
         builder.controlWithAccount({
             accountAlias: req.body.firstName + " " + req.body.lastName,
-            assetAlias: req.body.Stock2,
+            assetAlias: (req.body.Stock2).replace(/\s+/g, ''),
             amount: parseInt(req.body.quantity2)
         })
     }).then(issuance => {
         return _signer.sign(issuance)
     }).then(signed => {
         return client.transactions.submit(signed)
-    })  
+    })
+  }).then( f => {
+
+    googleStocks([(req.body.Stock1).replace(/\s+/g, '')], function(error, data) {
+    		var Stock1Total=data[0].l * req.body.quantity1;
+  	});
+    //var Stock1Total=data[0].l * req.body.quantity1;
+
+    googleStocks([(req.body.Stock2).replace(/\s+/g, '')], function(error, data) {
+    		var Stock2Total=data[0].l * req.body.quantity2;
+  	});
+
+    //Putting the total stock amount on the ledger
+    client.transactions.build(builder => {
+        builder.issue({
+            assetAlias: req.body.firstName + req.body.lastName+"Total",
+            amount: Stock2Total+Stock1Total
+        })
+        builder.controlWithAccount({
+            accountAlias: req.body.firstName + " " + req.body.lastName,
+            assetAlias: req.body.firstName + req.body.lastName+"Total",
+            amount: Stock2Total+Stock1Total
+        })
+    }).then(issuance => {
+        return _signer.sign(issuance)
+    }).then(signed => {
+        return client.transactions.submit(signed)
+    })
   }).catch(err =>
   process.nextTick(() => {throw err }),
   res.send({msg:err})
   )
 });
+module.exports = router;
